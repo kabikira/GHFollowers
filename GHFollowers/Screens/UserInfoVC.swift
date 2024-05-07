72dccc0a2f2ce64c884410e1de00c19c44582ad4
kabikira <kabikirar2@protonmail.com>
2024-05-08 01:49:56 +0900

    フォロワーをタップしてモダールを表示

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,29 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by koala panda on 2024/05/07.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+
+        print(username)
+    }
+
+
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+
+
+}
