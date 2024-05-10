624eceef48cfa2a4e0f3f3661a0ecb43b81feb0e
kabikira <kabikirar2@protonmail.com>
2024-05-10 13:49:18 +0900

    レイアウト追加

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by koala panda on 2024/05/10.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
