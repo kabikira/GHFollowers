8e723822b893d9eb4b0b24af29c83b4acb0fd664
kabikira <kabikirar2@protonmail.com>
2024-05-12 14:55:38 +0900

    GFitemInfoView追加

---
+++ GHFollowers/Custom Views/Views/GFitemInfoView.swift
@@ -0,0 +1,74 @@
+//
+//  GFitemInfoView.swift
+//  GHFollowers
+//
+//  Created by koala panda on 2024/05/12.
+//
+
+import UIKit
+
+enum ItemInfoType {
+    case repos, gists, followers, following
+}
+
+
+class GFitemInfoView: UILabel {
+
+    let symbolImageView = UIImageView()
+    let titleLabel = GFTitleLabel(textAligment: .left, fontSize: 14)
+    let conutLabel = GFTitleLabel(textAligment: .center, fontSize: 14)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    
+    private func configure() {
+        addSubview(symbolImageView)
+        addSubview(titleLabel)
+        addSubview(conutLabel)
+
+        NSLayoutConstraint.activate([
+            symbolImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            symbolImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            symbolImageView.widthAnchor.constraint(equalToConstant: 20),
+            symbolImageView.heightAnchor.constraint(equalToConstant: 20),
+
+            titleLabel.centerYAnchor.constraint(equalTo: symbolImageView.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: symbolImageView.trailingAnchor, constant: 12),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 18),
+
+            conutLabel.topAnchor.constraint(equalTo: symbolImageView.bottomAnchor, constant: 4),
+            conutLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            conutLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            conutLabel.heightAnchor.constraint(equalToConstant: 18),
+        ])
+    }
+
+
+    func set(itemInfoType: ItemInfoType, withConut count: Int) {
+        switch itemInfoType {
+        case .repos:
+            symbolImageView.image = UIImage(systemName: SFSymbols.repos)
+            titleLabel.text = "Public Repos"
+        case .gists:
+            symbolImageView.image = UIImage(systemName: SFSymbols.gists)
+            titleLabel.text = "Public gists"
+        case .followers:
+            symbolImageView.image = UIImage(systemName: SFSymbols.followers)
+            titleLabel.text = "Followers"
+        case .following:
+            symbolImageView.image = UIImage(systemName: SFSymbols.following)
+            titleLabel.text = "Following"
+        }
+
+        conutLabel.text = String(count)
+    }
+}
