84d4d6de87817d777e18a8d04e094a995908f248
kabikira <kabikirar2@protonmail.com>
2024-05-13 22:56:41 +0900

    DateFormatterで日付取得

---
+++ GHFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Date+Ext.swift
+//  GHFollowers
+//
+//  Created by koala panda on 2024/05/13.
+//
+
+import Foundation
+
+extension Date {
+
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        return dateFormatter.string(from: self)
+    }
+
+}
