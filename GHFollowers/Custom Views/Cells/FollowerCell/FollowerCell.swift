09d72eeed5482a5b5cb3d5614b7a5d25e004c9cf
kabikira <kabikirar2@protonmail.com>
2024-05-27 08:38:06 +0900

    SwiftUI導入

--- GHFollowers/Custom Views/Cells/FollowerCell/FollowerCell.swift
+++ GHFollowers/Custom Views/Cells/FollowerCell/FollowerCell.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 class FollowerCell: UICollectionViewCell {
 
@@ -25,8 +26,12 @@ class FollowerCell: UICollectionViewCell {
 
 
     func set(follower: Follower) {
-        avatarImageView.downloadImage(formURL: follower.avatarUrl)
-        usernameLabel.text = follower.login
+        if #available(iOS 16.0, *) {
+            contentConfiguration = UIHostingConfiguration { FollowerView(follower: follower) }
+        } else {
+            avatarImageView.downloadImage(formURL: follower.avatarUrl)
+            usernameLabel.text = follower.login
+        }
     }
 
 
