b675e642f413248c1c32fae38950d7945fb4d85c
kabikira <kabikirar2@protonmail.com>
2024-05-26 23:57:15 +0900

    お気に入りリストが空ときの画面表示修正

--- GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -106,6 +106,9 @@ extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
             guard let error = error else {
                 favorites.remove(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath], with: .left)
+                if self.favorites.isEmpty {
+                    self.showEmptyStateView(with: "No Favorites?\nAdd one on the follower screen.", in: self.view)
+                }
                 return
             }
             DispatchQueue.main.async {
